10f2efaf68d0e7297cd8a2a359f8e43a637a2420
petrovnicole1 <nicolepetrovhumber@gmail.com>
2024-11-27 21:50:26 -0500

    XML file

--- ComponentFramework/IrrklangAudio.cpp
+++ ComponentFramework/IrrklangAudio.cpp
@@ -1,31 +1,31 @@
-#include "iostream"
-#include "irrKlang.h"
-
-using namespace irrklang;
-
-class MyIrrklang {
-public:
-	void SoundPlayer() {
-		ISoundEngine* engine = createIrrKlangDevice();
-		if (!engine) {
-			return;
-		}
-
-		char i = 0;
-
-		while (i != 'q')
-		{
-			std::cout << "Press any key to play some sound, press 'q' to quit.\n";
-		engine->play2D("C:/Users/devon/Music/GameJam (Clockwork)/Alert.wav");
-		std::cin >> i; // wait for user to press some key
-		}
-
-
-		engine->drop();
-		return;
-
-	}
-
-
-	
-};
+//#include "iostream"
+//#include "irrKlang.h"
+//
+//using namespace irrklang;
+//
+//class MyIrrklang {
+//public:
+//	void SoundPlayer() {
+//		ISoundEngine* engine = createIrrKlangDevice();
+//		if (!engine) {
+//			return;
+//		}
+//
+//		char i = 0;
+//
+//		while (i != 'q')
+//		{
+//			std::cout << "Press any key to play some sound, press 'q' to quit.\n";
+//		engine->play2D("C:/Users/devon/Music/GameJam (Clockwork)/Alert.wav");
+//		std::cin >> i; // wait for user to press some key
+//		}
+//
+//
+//		engine->drop();
+//		return;
+//
+//	}
+//
+//
+//	
+//};
