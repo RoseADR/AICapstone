762b22a064af05fb508d872b4e91fd043be67dbb
RoseADR <devon1997@hotmail.com>
2025-04-03 12:33:09 -0400

    Update AnimationSystem.cpp

--- ComponentFramework/AnimationSystem.cpp
+++ ComponentFramework/AnimationSystem.cpp
@@ -1,1 +1,10 @@
 #include "AnimationSystem.h"
+
+void AnimationSystem::Update(const float deltaTime){
+
+	for (auto& actor : animationActors) {
+		Ref<AnimationComponent> ac = actor->GetComponent<AnimationComponent>();
+		
+	}
+
+}
