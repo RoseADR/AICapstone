0b0a93a919db7a93cbdb6cfc7b3cf0bfe8d21481
RoseADR <devon1997@hotmail.com>
2025-04-09 15:06:10 -0400

    set up for exe

--- ComponentFramework/SceneManager.cpp
+++ ComponentFramework/SceneManager.cpp
@@ -266,7 +266,7 @@ void SceneManager::Run() {
 			ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(20, 20));
 
 			if (ImGui::Button("Start Game", buttonSize)) {
-				BuildNewScene(SCENE_NUMBER::SCENE1);
+				BuildNewScene(SCENE_NUMBER::SCENE3);
 				/*engine->play2D("./Audio/SciButtonSound.mp3");
 				engine->setSoundVolume(0.1);*/
 				
