1f1c36abcf5740fdcd665923988f62aa33104518
Ramz5154 <ramz5154@gmail.com>
2025-04-09 21:45:26 -0400

    Revert "for exe"
    This reverts commit 502a2dc57784079844c136e18810d108212e9ab8.

--- ComponentFramework/SceneManager.cpp
+++ ComponentFramework/SceneManager.cpp
@@ -70,7 +70,7 @@ bool SceneManager::Initialize(std::string name_, int width_, int height_) {
 	}
 
 	/********************************   Default first scene   ***********************/
-	BuildNewScene(SCENE_NUMBER::SCENE0);
+	BuildNewScene(SCENE_NUMBER::SCENE3);
 	
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
